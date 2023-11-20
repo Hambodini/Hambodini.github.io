1b054514ffe4fef77687abfec21fea57b25e8406
Hambodini <54291353+Hambodini@users.noreply.github.com>
2023-11-20 16:49:13 -0600

    -attempt at adding image to canvas

--- assets/js/game1.js
+++ assets/js/game1.js
@@ -1,1 +1,5 @@
 var c = document.getElementById("game1");
+var ctx = c.getContext("2d");
+var img1 = document.createElement("cookie");
+img1.src = "images/cookiePic.jpg";
+ctx.drawImage(img, 10, 10);
