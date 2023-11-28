de3c42c48c7b63c7d21c942e3119bd774497441d
Hambodini <54291353+Hambodini@users.noreply.github.com>
2023-11-28 12:40:07 -0600

    -added code for upgrade buttons to work in html and js

--- assets/js/game1.js
+++ assets/js/game1.js
@@ -8,6 +8,12 @@ const worker2El = document.getElementById("w2")
 const worker3El = document.getElementById("w3")
 const worker4El = document.getElementById("w4")
 const worker5El = document.getElementById("w5")
+const research1El = document.getElementById("u1")
+const research2El = document.getElementById("u2")
+const research3El = document.getElementById("u3")
+const research4El = document.getElementById("u4")
+const research5El = document.getElementById("u5")
+const research6El = document.getElementById("u6")
 const errorEl = document.getElementById("error")
 //in-game tracking variables
 var crumbCount = 0
@@ -55,6 +61,37 @@ const worker5 = {
 
 const research1 = {
     name: "Second Hand Tools",
+    upgradeAmount: 1,
+    number: 0,
+    price: 0
+}
+const research2 = {
+    name: "Apprenticeship",
+    upgradeAmount: 1,
+    number: 0,
+    price: 0
+}
+const research3 = {
+    name: "Custom Tooling",
+    upgradeAmount: 1,
+    number: 0,
+    price: 0
+}
+const research4 = {
+    name: "Internship",
+    upgradeAmount: 1,
+    number: 0,
+    price: 0
+}
+const research5 = {
+    name: "6th Sense Training",
+    upgradeAmount: 1,
+    number: 0,
+    price: 0
+}
+const research6 = {
+    name: "Breaking Your Shackles",
+    upgradeAmount: 1,
     number: 0,
     price: 0
 }
@@ -117,11 +154,76 @@ function buy(buttonID) {
 
             break;
 
+        case "u1":
+            if (crumbCount >= research1.price) {
+                research1.number += 1
+                research1El.innerHTML = research1.name + " " + research1.number
+                crumbCount -= research1.price
+            } else {
+                errorMsg("you cant afford that!")
+            }
+
+            break;
+
+        case "u2":
+            if (crumbCount >= research2.price) {
+                research2.number += 1
+                research2El.innerHTML = research2.name + " " + research2.number
+                crumbCount -= research2.price
+            } else {
+                errorMsg("you cant afford that!")
+            }
+
+            break;
+
+        case "u3":
+            if (crumbCount >= research3.price) {
+                research3.number += 1
+                research3El.innerHTML = research3.name + " " + research3.number
+                crumbCount -= research3.price
+            } else {
+                errorMsg("you cant afford that!")
+            }
+
+            break;
+
+        case "u4":
+            if (crumbCount >= research4.price) {
+                research4.number += 1
+                research4El.innerHTML = research4.name + " " + research4.number
+                crumbCount -= research4.price
+            } else {
+                errorMsg("you cant afford that!")
+            }
+
+            break;
+
+        case "u5":
+            if (crumbCount >= research5.price) {
+                research5.number += 1
+                research5El.innerHTML = research5.name + " " + research5.number
+                crumbCount -= research5.price
+            } else {
+                errorMsg("you cant afford that!")
+            }
+
+            break;
+
+        case "u6":
+            if (crumbCount >= research6.price) {
+                research6.number += 1
+                research6El.innerHTML = research6.name + " " + research6.number
+                crumbCount -= research6.price
+            } else {
+                errorMsg("you cant afford that!")
+            }
+
+            break;
+
         default:
             break;
     }
 }
-
 function errorMsg(message) {
     if (errorEl.innerHTML == "") {
         errorEl.innerHTML = message
