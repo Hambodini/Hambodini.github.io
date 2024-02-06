74c4393d7bed53f0b90c8d7b5979950a05b29bc1
Hambodini <54291353+Hambodini@users.noreply.github.com>
2024-02-06 13:22:01 -0600

    added comments to todo js since i forgot to last time.

--- assets/js/todo_timer.js
+++ assets/js/todo_timer.js
@@ -1,3 +1,4 @@
+//task object
 class task {
     constructor(startTime, endTime, taskText) {
         this.startTime = startTime;
@@ -8,9 +9,19 @@ class task {
     endTime
     taskText
 };
+//list of all tasks
 taskList = {}
+//take forma data and add it to list
 function addTask() {
+    //get data from form
     taskRaw = $('form').serializeArray()
+    //make task obj and add to list of others
     task(taskRaw.startTime,taskRaw.endTime ,taskRaw.taskText)
+    //refresh display
+    refreshList()
     console.log(task)
+}
+//refresh html with taskes on list
+function refreshList() {
+
 }
